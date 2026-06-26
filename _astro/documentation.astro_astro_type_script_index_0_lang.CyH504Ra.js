fa07cdbf839732e9d488ba53ad8845fb79b76756
Alrenado <eugene1998@ukr.net>
2026-06-26 20:41:08 +0300

    fix: global file structure

--- _astro/documentation.astro_astro_type_script_index_0_lang.CyH504Ra.js
+++
@@ -1,1 +0,0 @@
-import{a,g as t}from"./translations.BbcGEfTL.js";document.addEventListener("astro:page-load",()=>{a(t())});
---
+++ _astro/documentation.astro_astro_type_script_index_0_lang.WbGCYt2e.js
@@ -0,0 +1,1 @@
+import{a,g as t}from"./translations._u0tZCsQ.js";document.addEventListener("astro:page-load",()=>{a(t())});
