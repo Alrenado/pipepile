29ecb848e39cea1c55811e1abfee4a22f34df032
Alrenado <eugene1998@ukr.net>
2026-06-26 20:41:08 +0300

    fix: global file structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import svgo from 'vite-plugin-svgo'
 
 const isProd = process.env.NODE_ENV === 'production'
-
-console.log('isProd:', isProd, 'base:', isProd ? '/pipepile' : '/')
 
 export default defineConfig({
     build:{
@@ -15,4 +13,19 @@ export default defineConfig({
     },
     site: 'https://Alrenado.github.io',
     base: '/pipepile',
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: 'Roboto',
+            cssVariable: '--font-roboto',
+            options: {
+                variants: [
+                    { weight: '300', style: 'normal', src: ['./src/assets/fonts/Roboto-Light.woff2', './src/assets/fonts/Roboto-Light.woff'] },
+                    { weight: '400', style: 'normal', src: ['./src/assets/fonts/Roboto-Regular.woff2', './src/assets/fonts/Roboto-Regular.woff'] },
+                    { weight: '500', style: 'normal', src: ['./src/assets/fonts/Roboto-Medium.woff2', './src/assets/fonts/Roboto-Medium.woff'] },
+                    { weight: '700', style: 'normal', src: ['./src/assets/fonts/Roboto-Bold.woff2', './src/assets/fonts/Roboto-Bold.woff'] },
+                ]
+            }
+        },
+    ],
 });
